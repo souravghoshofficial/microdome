66be600601793744ab7bbe73d8f2fec6d6c031ad
Abhijit-Rabidas <abhijit.rabidas.mca@gmail.com>
2025-05-07 11:00:41 +0530

    add user register route

--- Backend/controllers/user.controller.js
+++ Backend/controllers/user.controller.js
@@ -1,6 +1,51 @@
-const registerUser =  (req,res) =>{
-  const {name, email, password} = req.body;
-  console.log(name,email,password);
- }
+import { User } from "../models/user.model.js";
+import bcrypt from "bcrypt";
 
- export {registerUser};
+const registerUser = async (req, res) => {
+  // step1: get details from frontend
+  // step2: validate the data
+  // step3: check if user already exits or not
+  // step4: save the details in database
+  // step5: send response to frontend
+
+  const { name, email, password } = req.body;
+  // console.log(name,email,password);
+  // res.send("Successfully get the user details!");
+
+  if (!name || !email || !password) {
+    return res.send("All fields are required");
+  }
+
+  const exitedUser = await User.findOne({ email });
+
+  if (exitedUser) {
+    return res.send("User already exits");
+  }
+
+   const hashedPassword = await bcrypt.hash(password,10)
+
+    const user = await User.create({
+      name,
+      email,
+      password:hashedPassword
+  })
+
+  // const user = await User.create({
+  //   name,
+  //   email,
+  //   password,
+  // });
+
+  const createdUser = await User.findById(user._id);
+
+  if (!createdUser) {
+    throw new Error("Something went wrong");
+  }
+
+  return res
+    .status(201)
+    .json({ message: "User registered successfully" })
+    .send(user);
+};
+
+export { registerUser };
--- Backend/models/user.model.js
+++ Backend/models/user.model.js
@@ -1,4 +1,5 @@
-import mongoose, {Schema} from "mongoose";
+import mongoose, { Schema } from "mongoose";
+//import bcrypt from "bcrypt";
 
 const userSchema = new Schema(
   {
@@ -17,8 +18,17 @@ const userSchema = new Schema(
     password: {
       type: String,
       required: true,
-    }
+    },
+  },
+  {
+    timestamps: true,
   }
-)
+);
 
-export const User = mongoose.model("User",userSchema);
+// userSchema.pre("save", async function (next) {
+//   if (!this.isModified("password")) return next();
+//   this.password = bcrypt.hash(this.password, 10);
+//   next();
+// });
+
+export const User = mongoose.model("User", userSchema);
