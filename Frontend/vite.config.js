e54bbbaf80f89b1f635237d1f5dcb795790b3efe
souravghoshofficial <souravghosh.tech@gmail.com>
2025-05-11 22:50:52 +0530

    update: vite.config.js file

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -2,16 +2,20 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: 
-  [
+  plugins: [
     react(),
     tailwindcss(),
   ],
-  server:{
-    proxy:{
-      '/api' : 'https://microdome-backend.vercel.app'
-    },
+  server: {
+    proxy: {
+      '/api': {
+        target: 'https://microdome-backend.vercel.app',
+        changeOrigin: true,
+        secure: true,
+        rewrite: (path) => path.replace(/^\/api/, '') // Optional: strips `/api` if backend doesn't expect it
+      }
+    }
   }
 })
