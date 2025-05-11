91c52ecee86edf6d703801293033e51e5b8b2ad1
souravghoshofficial <souravghosh.tech@gmail.com>
2025-05-11 21:03:20 +0530

    modify: vite.config.js file

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -1,17 +1,17 @@
-import { defineConfig, loadEnv } from "vite";
-import react from "@vitejs/plugin-react";
-import tailwindcss from "@tailwindcss/vite";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
 
-export default ({ mode }) => {
-  process.env = { ...process.env, ...loadEnv(mode, process.cwd()) };
-
-  return defineConfig({
-    plugins: [react(), tailwindcss()],
-
-    server: {
-      proxy: {
-        "/api": parseInt(process.env.BACKEND_URL),
-      },
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: 
+  [
+    react(),
+    tailwindcss(),
+  ],
+  server:{
+    proxy:{
+      '/api' : 'https://microdome-backend.vercel.app'
     },
-  });
-};
+  }
+})
