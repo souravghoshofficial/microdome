d2d96b966f25ce0023bf19027c8a201207f713e2
souravghoshofficial <souravghosh.tech@gmail.com>
2025-05-25 11:15:17 +0530

    add: dotenv config in cloudinary.js file

--- Backend/utils/cloudinary.js
+++ Backend/utils/cloudinary.js
@@ -1,6 +1,10 @@
 import { v2 as cloudinary } from "cloudinary";
 import fs from "fs";
 
+import dotenv from "dotenv";
+
+dotenv.config()
+
 // Configuration
 cloudinary.config({
   cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
