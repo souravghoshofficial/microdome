f25e41a8e84577992fc532bffbf1f6e53c0eb0e0
souravghoshofficial <souravghosh.tech@gmail.com>
2025-06-13 23:57:56 +0530

    modify the ui of enroll.jsx and coursecard.jsx

--- Frontend/src/components/CourseCard.jsx
+++ Frontend/src/components/CourseCard.jsx
@@ -20,7 +20,7 @@ const CourseCard = ({
     <Link to={linkAddress} className="w-full cursor-pointer hover:-translate-y-3 transition-all duration-300 flex flex-col overflow-hidden text-black dark:bg-zinc-900 dark:text-white rounded-xl border border-zinc-900/10 dark:border-gray-700/25 bg-white shadow-lg shadow-gray-300 dark:shadow-[0_4px_12px_rgba(0,0,0,0.6)_0_1px_3px_rgba(255,255,255,0.05)]">
       <div className="w-full relative">
         <h5 className={`${courseTag ? "block" : "hidden"} absolute top-3 right-3 text-[9px] font-bold tracking-wide px-2 py-0.5 rounded-sm bg-slate-100 text-black`}>{courseTag}</h5>
-        <img className={`w-full object-cover object-center ${imageHeight}`} src={courseImg} alt="course image" />
+        <img className={`w-full object-cover object-center ${imageHeight}`} src={courseImg} alt={courseTitle} />
       </div>
       <div className="p-3 lg:p-4">
         <h2 className="mt-1 text-xl font-bold">{courseTitle}</h2>
@@ -31,16 +31,18 @@ const CourseCard = ({
         </div>
         <div className="mt-2 flex items-center justify-between">
           <div className="flex items-center gap-3">
-            <h3 className="text-lg font-semibold">₹ {discountedPrice}</h3>
-            <h3 className={`${actualPrice === discountedPrice ? "hidden" : "block"} text-sm line-through`}>
+            <h3 className="text-lg md:text-xl font-semibold">₹ {discountedPrice}</h3>
+            <h3 className={`${actualPrice === discountedPrice ? "hidden" : "block"} text-sm md:text-lg line-through`}>
               ₹ {actualPrice}
+              
             </h3>
+            <h3 className="text-sm -ml-1.5 text-gray-700 dark:text-gray-400">{type === "LIVE" ? "/monthly" : ""}</h3>
           </div>
           <div>
             <h4 className={`${discount ? "block" : "hidden"} text-right px-2 py-0.5 text-sm font-semibold bg-slate-200 dark:bg-white rounded-md text-black`}>{discount}% OFF</h4>
           </div>
         </div>
-        <Link to={linkAddress} className="mt-4 block w-full px-3 py-2 text-center text-lg font-semibold cursor-pointer bg-blue-600 text-white rounded-md">
+        <Link to={linkAddress} className="mt-4 block w-full px-3 py-2 text-center text-lg font-semibold cursor-pointer bg-blue-600 hover:bg-blue-700 text-white rounded-md">
           {btnText}
         </Link>
       </div>
--- Frontend/src/pages/Enroll.jsx
+++ Frontend/src/pages/Enroll.jsx
@@ -1,44 +1,64 @@
-import React from "react";
 import { Link } from "react-router";
-
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import {faCube} from '@fortawesome/free-solid-svg-icons'
+import { Rocket, BookOpen, Video } from "lucide-react";
 
 const Enroll = () => {
   return (
-    <div className="mt-8 w-full h-screen flex items-center justify-center">
-      <div className="w-[90%]">
-        <h4 className="text-center text-sm font-bold">Enroll</h4>
+    <div className="my-16 w-full flex items-center justify-center">
+      <div className="w-[90%] md:w-[81%]">
+        <h4 className="text-center text-sm font-semibold text-gray-400 dark:text-gray-500">Enroll</h4>
         <h2 className="mt-2 text-3xl md:text-4xl text-center font-bold">
-          Join Our Learning <span className="highlighted-text">Journey</span>
+          Join Our Learning <span className="text-highlighted">Journey</span>
         </h2>
-        <p className="mt-4 text-center">
+        <p className="mt-3 text-center text-gray-500 dark:text-gray-400">
           Follow these simple steps to start your learning.
         </p>
-        <div className="mt-16 w-full flex flex-col md:flex-row md:items-center md:justify-between">
-          <div className="w-full h-56 md:w-[45%] px-4 py-8 md:px-8 border">
-            <FontAwesomeIcon icon={faCube} className="text-xl md:text-2xl" />
-            <h2 className="mt-2 text-xl md:text-2xl font-bold">Get Started Now</h2>
-            <p>
-              Register on our platform to access a wealth of resources and
-              courses tailored for you.
-            </p>
-            <div className="mt-2 w-full flex gap-4">
-                <Link to="signup" className="px-3 py-1.5 border rounded-sm">Signup</Link>
-                <Link to ="courses" className="px-3 py-1.5 border rounded-sm border-highlighted bg-highlighted text-white">Enroll</Link>
+
+        <div className="mt-12 flex flex-col md:flex-row gap-6">
+          {/* CARD 1 */}
+          <div className="flex-1 p-6 border border-gray-950/[.1] dark:border-gray-50/[.1] dark:bg-gray-50/[.10] rounded-2xl shadow-sm flex flex-col justify-between">
+            <div>
+              <div className="w-12 h-12 md:h-14 md:w-14 flex items-center justify-center bg-blue-100 dark:bg-[#0F1D2B] rounded-full border border-[#109FE7]">
+                <Rocket className="w-5 h-5 md:w-6 md:h-6 text-[#109FE7]" />
+              </div>
+              <h3 className="mt-4 text-xl font-bold text-gray-900 dark:text-white">Get Started Now</h3>
+              <p className="mt-2 text-gray-600 dark:text-gray-400">
+                Register on our platform to access a wealth of resources and courses tailored for you.
+              </p>
+            </div>
+            <div className="mt-4 flex gap-3">
+              <Link to="signup" className="px-4 py-2 border border-gray-400 dark:border-gray-600 rounded-md text-sm hover:bg-gray-100 dark:hover:bg-gray-800">
+                Signup
+              </Link>
+              <Link to="courses" className="px-4 py-2 rounded-md text-sm border border-highlighted bg-highlighted text-white">
+                Enroll
+              </Link>
             </div>
           </div>
-          <div className="mt-4 h-56 md:mt-0 w-full md:w-[25%] px-4 py-8 md:px-8 border">
-            <FontAwesomeIcon icon={faCube} className="text-xl md:text-2xl" />
-            <h2 className="mt-2 text-xl md:text-2xl font-bold">Access Courses and Materials</h2>
-            <p>Start learning with our expert-led 
-            courses today!</p>
+
+          {/* CARD 2 */}
+          <div className="flex-1 p-6 border border-gray-950/[.1] dark:border-gray-50/[.1] dark:bg-gray-50/[.10] rounded-2xl shadow-sm flex flex-col justify-between">
+            <div>
+              <div className="w-12 h-12 md:h-14 md:w-14 flex items-center justify-center bg-green-100 dark:bg-[#0F1D2B] rounded-full border border-[#23C45C]">
+                <BookOpen className="w-5 h-5 md:w-6 md:h-6 text-[#23C45C]" />
+              </div>
+              <h3 className="mt-4 text-xl font-bold text-gray-900 dark:text-white">Access Courses and Materials</h3>
+              <p className="mt-2 text-gray-600 dark:text-gray-400">
+                Start learning with our expert-led courses today!
+              </p>
+            </div>
           </div>
-          <div className="mt-4 h-56 md:mt-0 w-full md:w-[25%] px-4 py-8 md:px-8 border">
-            <FontAwesomeIcon icon={faCube} className="text-xl md:text-2xl" />
-            <h2 className="mt-2 text-xl md:text-2xl font-bold">Join Live Classes Anytime</h2>
-            <p>Engage with instructors and peers 
-            in real-time.</p>
+
+          {/* CARD 3 */}
+          <div className="flex-1 p-6 border border-gray-950/[.1] dark:border-gray-50/[.1] dark:bg-gray-50/[.10] rounded-2xl shadow-sm  flex flex-col justify-between">
+            <div>
+              <div className="w-12 h-12 md:h-14 md:w-14 flex items-center justify-center bg-red-100 dark:bg-[#0F1D2B] rounded-full border border-[#EF476F]">
+                <Video className="w-5 h-5 md:w-6 md:h-6 text-[#EF476F]" />
+              </div>
+              <h3 className="mt-4 text-xl font-bold text-gray-900 dark:text-white">Join Live Classes Anytime</h3>
+              <p className="mt-2 text-gray-600 dark:text-gray-400">
+                Engage with instructors and peers in real-time.
+              </p>
+            </div>
           </div>
         </div>
       </div>
