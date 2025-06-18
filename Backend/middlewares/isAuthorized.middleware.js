18b61431660e6898ed8b47290b4fbdb4d0473ae0
souravghoshofficial <souravghosh.tech@gmail.com>
2025-06-18 11:22:04 +0530

    modify course auth middleware

--- Backend/middlewares/isAuthorized.middleware.js
+++ Backend/middlewares/isAuthorized.middleware.js
@@ -1,6 +1,6 @@
 import mongoose from "mongoose"
-import User from "../models/user.model.js"
-import Course from "../models/course.model.js"
+import {User} from "../models/user.model.js"
+import {Course} from "../models/course.model.js"
 
 export const isEnrolledInCourse = async (req, res, next) => {
   try {
