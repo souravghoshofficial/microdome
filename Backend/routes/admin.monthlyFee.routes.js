b15fd4a5b0d2d7dfbb62ed8e093622e2808272d4
souravghoshofficial <souravghosh.tech@gmail.com>
2026-02-11 17:04:14 +0530

    add: the init monnly fee for new year route

--- Backend/routes/admin.monthlyFee.routes.js
+++ Backend/routes/admin.monthlyFee.routes.js
@@ -1,11 +1,12 @@
 import { Router } from "express";
 
-import { markMonthlyFeePaid, markMonthlyFeeUnpaid, getCourseMonthlyFeesByYear } from "../controllers/admin.monthlyFee.controller.js";
+import { markMonthlyFeePaid, markMonthlyFeeUnpaid, getCourseMonthlyFeesByYear, initMonthlyFeeForYear } from "../controllers/admin.monthlyFee.controller.js";
 import { authorizedRoles } from "../middlewares/authorizedRoles.middleware.js";
 
 const router = Router();
 
 router.get("/course/:courseId", authorizedRoles("admin"), getCourseMonthlyFeesByYear);
+router.post("/init-year", authorizedRoles("admin"), initMonthlyFeeForYear);
 
 router.patch("/mark-paid", authorizedRoles("admin"), markMonthlyFeePaid);
 router.patch("/mark-unpaid", authorizedRoles("admin"), markMonthlyFeeUnpaid);
